64d001a8dcd9450d2ae20e451208b8a7f318f7e1
paul.ilhe@vector8.com <paul.ilhe@vector8.com>
2026-03-16 20:32:17 +0100

    feat: add sonner toast library

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'sonner'
 import App from './App'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Toaster position="top-right" richColors />
     <App />
   </React.StrictMode>
 )
